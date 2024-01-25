d6bb0f81499271a0674109c5de8b9e0b6df8ea71
José Carlos Ferrari <licoverpa321@gmail.com>
2024-01-24 23:17:13 -0300

    connecting ownerinfo page to backend

---
+++ src/components/OwnerInfo.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+
+const OwnerInfo = (props) => {
+  const [data, setData] = useState(null);
+  const { ownerName } = props;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8190/api/owners/teste");
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const correctOwner = data ? data.filter((e) => e.name === ownerName) : null;
+
+  return data ? (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        textAlign: "center",
+        justifyContent: "space-evenly",
+      }}
+    >
+      {correctOwner.map((e, index) => (
+        <div key={index} style={{ marginRight: "2em" }}>
+          <h1 style={{ color: "red" }}>{e.name}</h1>
+          <p style={{ color: "red" }}>{e.cellphone}</p>
+          <p style={{ color: "red" }}>{e.address}</p>
+        </div>
+      ))}
+    </div>
+  ) : (
+    <p>Loading...</p>
+  );
+};
+
+export default OwnerInfo;
--- src/pages/OwnerPage.jsx
+++ src/pages/OwnerPage.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Navbar from "../components/NavBar";
+import OwnerInfo from "../components/OwnerInfo";
+import { useParams } from "react-router-dom";
 
 const OwnerPage = () => {
+  const { ownerName } = useParams();
   return (
     <div style={{ backgroundColor: "#000000", height: "100vh" }}>
       <div>
         <Navbar />
       </div>
-      <div></div>
+      <div>
+        <OwnerInfo ownerName={ownerName} />
+      </div>
     </div>
   );
 };
