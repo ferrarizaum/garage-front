81a39f86a214767f4254c4a3a386f0a5bb401a81
José Carlos Ferrari <licoverpa321@gmail.com>
2024-02-01 22:14:26 -0300

    fixing rendering

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Card = (props) => {
-  const { model, description } = props;
-  return (
+const Card = () => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8190/api/cars/teste");
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const getRandomNumber = () => Math.random() - 0.5;
+
+  const shuffledData = data?.slice().sort(getRandomNumber);
+
+  return data ? (
     <div
       style={{
         backgroundColor: "#000000",
@@ -17,14 +35,22 @@ const Card = (props) => {
           flexDirection: "column",
         }}
       >
-        <div>
-          <h1 style={{ fontSize: "4em" }}>{model}</h1>
-        </div>
-        <div style={{ marginTop: 8 }}>
-          <h2>{description}</h2>
+        <div style={{ display: "flex" }}>
+          {shuffledData.slice(0, 4).map((e) => (
+            <div style={{ padding: "4em" }} key={e.id}>
+              <div>
+                <h1 style={{ fontSize: "4em" }}>{e.model}</h1>
+              </div>
+              <div style={{ marginTop: 8 }}>
+                <h2>{e.description}</h2>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
+  ) : (
+    <p>Loading...</p>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Navbar from "../components/NavBar";
 import Card from "../components/Card";
 
 const Home = () => {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://localhost:8190/api/cars/teste");
-        const result = await response.json();
-        setData(result);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  const getRandomNumber = () => Math.random() - 0.5;
-
-  const shuffledData = data?.slice().sort(getRandomNumber);
-
-  return data ? (
+  return (
     <div style={{ backgroundColor: "#000000", height: "100vh" }}>
       <div>
         <Navbar />
@@ -35,15 +16,11 @@ const Home = () => {
           alignItems: "center",
         }}
       >
-        {shuffledData.slice(0, 4).map((e) => (
-          <div style={{ padding: "4em" }} key={e.id}>
-            <Card model={e.model} description={e.description} />
-          </div>
-        ))}
+        <div>
+          <Card />
+        </div>
       </div>
     </div>
-  ) : (
-    <p>Loading...</p>
   );
 };
 
